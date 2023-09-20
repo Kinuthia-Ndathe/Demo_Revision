8e1ebbd73cd321c11d2064c2931b5e2374e0ea3c
v-alkinu <v-alkinu@DESKTOP-D9KC0HD>
2023-09-20 20:12:44 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Threading.Channels;
+using System.Transactions;
+
+namespace Demo_Revision
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int YearOfBirth = 0;
+            int Age = 0;
+
+            Console.WriteLine("Enter your Year of Birth please...");
+            string input = Console.ReadLine();
+            YearOfBirth = int.Parse(input);
+
+            const int CurrentYear = 2023;
+
+            Age = CurrentYear - YearOfBirth;
+
+            Console.WriteLine($"Your age is {Age}");
+
+        }
+    }
+}
